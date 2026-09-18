d0067011c8a167e3e3f1adde3ee10621fdb026ef
Watashi85 <6910122113013@pnru.ac.th>
2026-09-18 11:07:22 +0700

    Update Message

--- Hello1.java
+++ Hello1.java
@@ -1,7 +1,5 @@
 public class Hello1 {
     public static void main(String[] args) {
-        System.out.println("Hello1");
-        System.out.println("Hello2");
-        System.out.println("Hello3");
+        System.out.println("I love PNRU");
     }
 }
