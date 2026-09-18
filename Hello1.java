5bf929fa412509c2df09791f79d923839b0c5442
Watashi85 <6910122113013@pnru.ac.th>
2026-09-18 11:02:09 +0700

    My Editting -2

--- Hello1.java
+++ Hello1.java
@@ -1,7 +1,7 @@
 public class Hello1 {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello");
+        System.out.println("Hello1");
+        System.out.println("Hello2");
+        System.out.println("Hello3");
     }
 }
